70fdb1f46b8c79ab83fee3b7c3cf21b63b5e1850
Aaron-Serpilin <serpilin.aaron@gmail.com>
2026-02-23 10:13:04 +0100

    🔥 chore(infra): remove PostgreSQL, embedding pipeline, and DB-dependent scripts, as well as local docker setup. Can work directly with the .csvs

---
+++ utils/pubsub.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+from google.cloud import pubsub_v1
+
+load_dotenv()
+
+publisher = pubsub_v1.PublisherClient()
+
+def publish_to_topic (topic_id: str,
+                      message: str):
+    
+    topic_name = 'projects/{project_id}/topics/{topic}'.format(
+        project_id=os.getenv('GC_PROJECT_ID'),
+        topic=topic_id,  
+    )
+    
+    data = message.encode("utf-8")
+    result = publisher.publish(topic_name, data)
+
+    result.result()
+
+    print(f"Message {result} has been published.")
